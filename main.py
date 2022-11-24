a7408624da867905ff0a209b06d8d820f6a35957
Ray <89009692+ray-concepts@users.noreply.github.com>
2022-11-25 00:27:34 +0530

    final checks

--- main.py
+++ main.py
@@ -1,30 +1,42 @@
+#Initialise Clover
+import clover_init
+clover_init.db_init()
+#Import the rest of the modules
+import sqlite3 as sql
+import time
 import sys ,os
+import re
+#PyQt Modules
 from PyQt6.uic import loadUi
 from PyQt6 import QtWidgets
 from PyQt6.QtWidgets import QApplication, QDialog, QLabel, QProgressBar
 from PyQt6.QtGui import QIcon, QPixmap, QFont, QFontDatabase 
 from PyQt6.QtCore import *
-import re
+#Custom modules
 import cipher_module
-import vault8_stylesheets as styles
-import sqlite3 as sql
-import time
-import clover_init
+import clover_stylesheets as styles
 
-clover_init.package_init()
-clover_init.db_init()
+#Conncection to SQLite DB
 activedb = sql.connect("CLOVER_DB.db")
 cur = activedb.cursor()
-
 time.sleep(2)
+
+#Application Start
+#Login Screen
 class LoginScreen(QDialog): # Login Screen #AES DECRYPTION 
     def __init__(self):
+        #Load the UI
         super(LoginScreen, self).__init__()
         label = QLabel(self)
         pixmap = QPixmap("rsrc/clover_login_bg_v1.png")
         label.setPixmap(pixmap)
         loadUi("clover_login.ui", self)
-
+        #Set the window title
+        self.setWindowTitle("Clover | Login")
+        #Set the window icon
+        self.setWindowIcon(QIcon("rsrc/clover_icon.png"))
+        
+        #Assign functions to buttons
         self.loginbutton.clicked.connect(self.loginfunction)
         self.signupbutton.clicked.connect(self.gotocreate)
 
@@ -34,43 +46,47 @@ class LoginScreen(QDialog): # Login Screen #AES DECRYPTION
         self.usrnmfield.setStyleSheet(styles.form_style)
         self.pwdfield.setStyleSheet(styles.form_style)
 
+    #Login Function
     def loginfunction(self):
         global user #global variable for the user
-        user = self.usrnmfield.text().lower()
-        password = self.pwdfield.text()
+        user = self.usrnmfield.text().lower() #Get the username
+        password = self.pwdfield.text() #Get the password
+        #Check if the username exists
         cur.execute(f"SELECT COUNT(CLOVER_USRNM) FROM CLOVER_MASTERDB WHERE CLOVER_USRNM = '{user}'")
         row_count = cur.fetchall()
         def login():
-            if len(user) == 0 or len(password) == 0:
+            if len(user) == 0 or len(password) == 0: #Check if the fields are empty
                 self.alertbox.setText("Please Input all Fields!")
                 timer.singleShot(2000, self.clear_alertbox)
-            elif row_count[0][0] == 0:
+            elif row_count[0][0] == 0: #Check if the username exists
                 self.alertbox.setText("Login Credentials Not Found.\nPlease Create an Account First!")
                 timer.singleShot(2000, self.clear_alertbox)
-            else:
+            else: 
+                #Check if the password is correct
                 cur.execute(f"SELECT nKEY FROM CLOVER_MASTERDB WHERE CLOVER_USRNM = '{user}' or CLOVER_EMAIL = '{user}'")
                 special_key = cur.fetchone()
                 hashed_pwd = cipher_module.hash(password, special_key[0])
                 cur.execute(f"SELECT CLOVER_PWD FROM CLOVER_MASTERDB WHERE CLOVER_USRNM = '{user}' or CLOVER_EMAIL = '{user}'")
                 result = cur.fetchall()
-                if len(result) == 0:
+                if len(result) == 0: 
                     self.alertbox.setText("Invalid Username or Email!")
                     timer.singleShot(2000, self.clear_alertbox)
-                elif hashed_pwd == result[0][0]:
+                elif hashed_pwd == result[0][0]: #If the password is correct
                     self.alertbox.setText("Authenticated!\nPress Login again to continue.")
-                    timer.singleShot(5000, self.clear_alertbox)
+                    timer.singleShot(2000, self.clear_alertbox)
                     self.loginbutton.clicked.connect(self.gotodashboard)
-                else:
+                else: #If the password is incorrect
                     self.alertbox.setText("Invalid Password!")
                     timer.singleShot(2000, self.clear_alertbox)
-        login()
+        login() #Call the login function
 
-    def gotocreate(self): #[WIP]
+
+    def gotocreate(self): #signup page link
         signup = SignUpScreen()
         widget.addWidget(signup)
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
-    def gotodashboard(self): #[WIP]
+    def gotodashboard(self): #dashboard page link
         dashboard = DashboardScreen()
         widget.addWidget(dashboard)
         widget.setCurrentIndex(widget.currentIndex() + 1)
@@ -79,7 +95,7 @@ class LoginScreen(QDialog): # Login Screen #AES DECRYPTION
         self.alertbox.setText("")
 
 
-#THERE ARE STILL BUGS IN THE FLOW OF THE CODE OF THIS MF! I NEED TO FIX IT ASAP!
+#Signup Screen
 class SignUpScreen(QDialog): # Sign Up Screen #FIX SPECIAL CHARACTERS ENTRY ISSUE and password strength checker and aes encrption
     def __init__(self):
         super(SignUpScreen, self).__init__()
@@ -140,7 +156,7 @@ class SignUpScreen(QDialog): # Sign Up Screen #FIX SPECIAL CHARACTERS ENTRY ISSU
             salt = cipher_module.caesar_encrypt(user, 8)
             hashed_pwd = cipher_module.hash(password, salt)
             self.alertbox.setText(f"{user_db}, {email}, {password}, {password_b}, {hashed_pwd}") #testing
-            cur.execute(f"CREATE TABLE if not exists {user_db}(email varchar(50), appname varchar(20) NOT NULL, app_password varchar(100) NOT NULL, app_enKEY varchar(100) NOT NULL);")
+            cur.execute(f"CREATE TABLE if not exists {user_db}(email varchar(50), appname varchar(20) NOT NULL, app_password varchar(100) NOT NULL);")
             cur.execute(f"INSERT INTO CLOVER_MASTERDB(CLOVER_USRNM, CLOVER_EMAIL, CLOVER_PWD, nKEY) VALUES('{user}','{email}', '{hashed_pwd}', '{salt}')")
             activedb.commit()
             self.alertbox.setText("Account Created Successfully!\nRedirecting to Login Screen...")
@@ -157,7 +173,7 @@ class SignUpScreen(QDialog): # Sign Up Screen #FIX SPECIAL CHARACTERS ENTRY ISSU
 
 
 
-class DashboardScreen(QDialog): # Dashboard Screen | AND ADD DATABASE TO LIST
+class DashboardScreen(QDialog): # Dashboard Screen 
     def __init__(self):
         super(DashboardScreen, self).__init__()
         label = QLabel(self)
